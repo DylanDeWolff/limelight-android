24ea88f36e9b47190c6a0c26571bc427b1f8f737
Dylan de Wolff <dylandewolff@gmail.com>
2015-02-07 12:15:33 +0100

    Will now move cursor on each tap of the screen.
    Will now hold the mouse button when touch event is held until release.
    
    Added calibration mode when touch event is held on bottom right of the screen. This allows the user to move the cursor to the center of the screen.

--- app/src/main/java/com/limelight/binding/input/TouchContext.java
+++ app/src/main/java/com/limelight/binding/input/TouchContext.java
@@ -1,30 +1,34 @@
 package com.limelight.binding.input;
 
+import android.graphics.Point;
+
 import com.limelight.nvstream.NvConnection;
 import com.limelight.nvstream.input.MouseButtonPacket;
 
 public class TouchContext {
-	private int lastTouchX = 0;
-	private int lastTouchY = 0;
+    private static final int TAP_MOVEMENT_THRESHOLD = 10;
+    private static final int TAP_TIME_THRESHOLD = 250;
+    private final NvConnection conn;
+    private final int actionIndex;
+    private final double xFactor;
+    private final double yFactor;
+    private final Point screenSize;
+    private int lastTouchX = 0;
+    private int lastTouchY = 0;
 	private int originalTouchX = 0;
 	private int originalTouchY = 0;
 	private long originalTouchTime = 0;
     private boolean cancelled;
-	
-	private final NvConnection conn;
-	private final int actionIndex;
-    private final double xFactor;
-    private final double yFactor;
-	
-	private static final int TAP_MOVEMENT_THRESHOLD = 10;
-	private static final int TAP_TIME_THRESHOLD = 250;
-	
-	public TouchContext(NvConnection conn, int actionIndex, double xFactor, double yFactor)
-	{
-		this.conn = conn;
+    private boolean calibrating = false;
+    private boolean holding = false;
+
+    public TouchContext(NvConnection conn, int actionIndex, double xFactor, double yFactor, Point screenSize) {
+        this.conn = conn;
 		this.actionIndex = actionIndex;
         this.xFactor = xFactor;
         this.yFactor = yFactor;
+        this.screenSize = screenSize;
+
 	}
 
     public int getActionIndex()
@@ -55,8 +59,9 @@ public class TouchContext {
 	
 	public boolean touchDownEvent(int eventX, int eventY)
 	{
-		originalTouchX = lastTouchX = eventX;
-		originalTouchY = lastTouchY = eventY;
+        moveEvent(eventX, eventY);
+        originalTouchX = lastTouchX = eventX;
+        originalTouchY = lastTouchY = eventY;
 		originalTouchTime = System.currentTimeMillis();
         cancelled = false;
 
@@ -65,14 +70,13 @@ public class TouchContext {
 	
 	public void touchUpEvent(int eventX, int eventY)
 	{
+        byte buttonIndex = getMouseButtonIndex();
         if (cancelled) {
             return;
         }
 
 		if (isTap())
 		{
-			byte buttonIndex = getMouseButtonIndex();
-			
 			// Lower the mouse button
 			conn.sendMouseButtonDown(buttonIndex);
 			
@@ -84,15 +88,38 @@ public class TouchContext {
 			
 			// Raise the mouse button
 			conn.sendMouseButtonUp(buttonIndex);
-		}
-	}
-	
-	public boolean touchMoveEvent(int eventX, int eventY)
+        } else if (calibrating) {
+            calibrating = false;
+            lastTouchX = Math.round(screenSize.x / 2);
+            lastTouchY = Math.round(screenSize.y / 2) - 10;
+        } else if (holding) {
+            conn.sendMouseButtonUp(buttonIndex);
+            holding = false;
+        }
+    }
+
+    public boolean touchMoveEvent(int eventX, int eventY)
     {
-        if (eventX != lastTouchX || eventY != lastTouchY)
-		{
-			// We only send moves for the primary touch point
-			if (actionIndex == 0) {
+        if (calibrating) {
+            moveEvent(eventX, eventY);
+        } else if ((eventX <= screenSize.x + 100 && eventX >= screenSize.x - 100) && (eventY <= screenSize.y + 100 && eventY >= screenSize.y - 100)) {
+            calibrating = true;
+        } else {
+            if (holding) {
+                moveEvent(eventX, eventY);
+            } else {
+                byte buttonIndex = getMouseButtonIndex();
+                conn.sendMouseButtonDown(buttonIndex);
+                holding = true;
+            }
+        }
+        return true;
+    }
+
+    private boolean moveEvent(int eventX, int eventY) {
+        if (eventX != lastTouchX || eventY != lastTouchY) {
+            // We only send moves for the primary touch point
+            if (actionIndex == 0) {
                 int deltaX = eventX - lastTouchX;
                 int deltaY = eventY - lastTouchY;
 
@@ -100,15 +127,15 @@ public class TouchContext {
                 deltaX = (int)Math.round((double)deltaX * xFactor);
                 deltaY = (int)Math.round((double)deltaY * yFactor);
 
-				conn.sendMouseMove((short)deltaX, (short)deltaY);
-			}
-			
-			lastTouchX = eventX;
-			lastTouchY = eventY;
-		}
-		
-		return true;
-	}
+                conn.sendMouseMove((short) deltaX, (short) deltaY);
+            }
+
+            lastTouchX = eventX;
+            lastTouchY = eventY;
+        }
+
+        return true;
+    }
 
     public void cancelTouch() {
         cancelled = true;
